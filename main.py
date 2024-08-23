faf1f35ea83c23e8fe9c0e63df98d31e2333cf97
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-23 16:51:38 +0545

    Updated'

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-import json,datetime
+import json
+import datetime
+import csv
 
-
-
+# Load data from JSON files
 with open('nepseindexvalue.json') as f:
     datavalue = json.load(f)
-
 
 with open('nepsetime.json') as f:
     nepsetime = json.load(f)
 
+# Open the CSV file for writing
+with open('NepseData.csv', 'w', newline='') as csvfile:
+    # Create a CSV writer object
+    writer = csv.writer(csvfile)
 
-with open('NepseData.csv', 'w') as f:
-    f.write('Time,Value\n')
-    for i in range(10):
+    # Write the header row
+    writer.writerow(['Time', 'Value'])
+
+    # Write the data rows
+    for i in range(len(datavalue)):
         date = datetime.datetime.utcfromtimestamp(nepsetime[i]).strftime('%Y-%m-%d %H:%M:%S')
-        f.write(str(date) + ',' + str(datavalue[i]) + '\n')
+        print("Time : " + date + " Value : " + str(datavalue[i]))
+        writer.writerow([date, datavalue[i]])
+
+print("Data has been successfully written to NepseData.csv")
